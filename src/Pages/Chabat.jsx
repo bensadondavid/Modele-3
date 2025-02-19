6c5784ee0e7d04c674f796d5990dccd6265fcb86
David <bensadondavidn@gmai.com>
2025-02-19 18:12:30 +0100

    changes

--- src/Pages/Chabat.jsx
+++ src/Pages/Chabat.jsx
@@ -5,7 +5,11 @@ function Chabat(activeLanguage, handleLanguage){
     return(
 
         <div className="chabat">
-            
+            <div className="chabat-fleur">
+                    <img src="fleur-chabat-haut.png" className='fleur-chabat-haut' />
+                    <h1 className="chabat-titre">Le Chabat</h1>
+                    <img src="fleur-chabat-bas.png" className="fleur-chabat-bas" />
+                </div>
         </div>
 
     )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import Houppa from "./Houppa"
+import Chabat from "./Chabat"
 
 function Home(activeLanguage, handleLanguage){
 
     return(
 
         <div className="home">
+            <img src="kol-sasson-bleu.png" className="kol-sasson" />
             <img src="logo-bleu.png" className="logo-bleu" />
             <Houppa />
+            <Chabat />
         </div>
 
     )
