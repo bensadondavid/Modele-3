001af8fddc2cb3ca5c72e6010b2de0b2b4aace77
David <bensadondavidn@gmai.com>
2025-02-25 22:25:21 +0100

    changes

--- src/Pages/Entry.jsx
+++ src/Pages/Entry.jsx
@@ -11,6 +11,13 @@ function Entry(){
 
         <div className="entry">
             <p className="bsd">בס"ד</p>
+            <div className="logo-entry">
+                <img src="logo-av.png" className='logo-av-entry' />
+                <p className='entry-date'>03 . 06 . 2024</p>
+            </div>
+            <p className="entry-noms">Victoria <span className='and'>&</span> Alone</p>
+            <CountDown />
+            <Link className='invit-link'><button className='invit-button'>Voir l'invitation</button></Link>
         </div>
 
     )
